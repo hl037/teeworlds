609ee5282af7359e9927369b760526db666ff38f
Shahan <maxusmic@gmail.com>
2011-12-26 19:17:41 +0400

    Bracket for successful compiling

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -1546,6 +1546,7 @@ void CCharacter::DDRacePostCoreTick()
 		// handle Anti-Skip tiles
 		std::list < int > Indices = GameServer()->Collision()->GetMapIndices(m_PrevPos, m_Pos);
 		if(!Indices.empty())
+		{
 			//iDDRace
 			CCharacter* pDummyChar = GameServer()->GetPlayerChar(g_Config.m_SvMaxClients - 1 - GetPlayer()->GetCID());
 			//this huge condition blocks handling tiles between owner and dummy on /dc
@@ -1560,6 +1561,7 @@ void CCharacter::DDRacePostCoreTick()
 				HandleTiles(*i);
 				//dbg_msg("Running","%d", *i);
 			}
+		}
 		else
 		{
 			HandleTiles(CurrentIndex);
